7ea9f741ad44dbe0d478de61d3c01c8e0b88ebbc
kamal kumar <kamalkumar1752000@gmail.com>
2025-01-05 23:07:49 +0530

    Cleaned up state variables

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -5,16 +5,17 @@ import Pokemon from '../Pokemon/Pokemon';
 
 function PokemonList() {
 
-    const [pokemonList, setPokemon] = useState([]);
     const DEFAULT_URL = 'https://pokeapi.co/api/v2/pokemon';
-    const [PokedexURL,setPokedexURL] = useState(DEFAULT_URL);
-    const [NextURL,setNextURL] = useState(DEFAULT_URL);
-    const [PreviousURL,setPreviousURL] = useState(DEFAULT_URL);
+
+    const [PokemonListState,setPokemonListState] = useState({
+        pokemonList : [],
+        PokedexURL : DEFAULT_URL,
+        NextURL : DEFAULT_URL,
+        PreviousURL : DEFAULT_URL
+})
 
     async function DownloadPokemonList() {
-        const response = await axios.get(PokedexURL ? PokedexURL : DEFAULT_URL);
-        setNextURL(response.data.next);
-        setPreviousURL(response.data.previous);
+        const response = await axios.get(PokemonListState.PokedexURL ? PokemonListState.PokedexURL : DEFAULT_URL);
         const PokemonListResults = response.data.results;
         const PokemonPromises = PokemonListResults.map((Pokemon) => axios.get(Pokemon.url));
         const PokemonListData = await axios.all(PokemonPromises);
@@ -27,12 +28,12 @@ function PokemonList() {
                 type: pokemon.types
             }
         })
-        setPokemon(PokemonFinalList);
+    setPokemonListState({...PokemonListState,pokemonList:PokemonFinalList,NextURL:response.data.next,});
     }
 
     useEffect(() => {
         DownloadPokemonList();
-    }, [PokedexURL])
+    }, [PokemonListState.PokedexURL])
 
 
 
@@ -42,11 +43,11 @@ function PokemonList() {
                 <h1 className='pokemon-list-header'>Pokemon List</h1>
             </div>
             <div className='page-controls'>
-                <button onClick={()=> setPokedexURL(PreviousURL)}>Previous</button>
-                <button onClick={()=> setPokedexURL(NextURL)}>Next</button>
+                <button onClick={()=> setPokemonListState({...PokemonListState,PokedexURL:PokemonListState.PreviousURL})}>Previous</button>
+                <button onClick={()=> setPokemonListState({...PokemonListState,PokedexURL:PokemonListState.NextURL})}>Next</button>
             </div>
             <div className='pokemon-list'>
-                {pokemonList.map(pokemon => <Pokemon key={pokemon.id} name={pokemon.name} url={pokemon.image} id={pokemon.id}/>)}
+                {PokemonListState.pokemonList.map(pokemon => <Pokemon key={pokemon.id} name={pokemon.name} url={pokemon.image} id={pokemon.id}/>)}
             </div>
         </div>
     )
